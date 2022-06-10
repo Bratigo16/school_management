9eba59b9dd0d4a0ef0ccf9d67e5c6677c016bc8b
Alizwa Solibanzi <220001499@mycput.ac.za>
2022-06-10 14:55:47 +0200

    Implementation of builder pattern on City and Name

---
+++ src/main/java/za/ac/cput/school_management/domain/City.java
@@ -0,0 +1,86 @@
+package za.ac.cput.school_management.domain;
+
+import org.jetbrains.annotations.NotNull;
+
+/*
+ *Author :Alizwa Solibanzi
+ * Student no: 220001499
+ * APPLICATION DEVELOPMENT PRACTICE 3
+ * ASSIGNMNET 2
+ *
+ */
+public class City {
+    private String id;
+    private  String name;
+    private String  country;
+
+   public City (Builder builder){
+       this.id = builder.id;
+       this.name = builder.name;
+       this.country = builder.country;
+   }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+    public class Builder {
+        private String id;
+        private String name;
+        private String country;
+
+        public Builder setId(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder setCountry(String country) {
+            this.country = country;
+            return this;
+        }
+
+        public Builder copy( City city) {
+            this.id =city.id ;
+            this.name = city.name;
+            this.country = city.country;
+            return  this;
+        }
+        public City build(){
+            return  new City(this);
+        }
+
+    }
+}
--- src/main/java/za/ac/cput/school_management/domain/Name.java
+++ src/main/java/za/ac/cput/school_management/domain/Name.java
@@ -1,4 +1,79 @@
 package za.ac.cput.school_management.domain;
-
+/*
+ *Author :Alizwa Solibanzi
+ * Student no: 220001499
+ * APPLICATION DEVELOPMENT PRACTICE 3
+ * ASSIGNMNET 2
+ *
+ */
 public class Name {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+
+     public Name(Builder builder){
+         this.firstName = builder.firstName;;
+         this.middleName = builder.middleName;;
+         this.lastName = builder.lastName;
+     }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Name{" +
+                "firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+    public class Builder{
+        private String firstName;
+        private String middleName;
+        private String lastName;
+
+        public Builder setFirstName(String firstName){
+            this.firstName = firstName;
+            return this;
+        }
+        public Builder setMiddleName(String middleName){
+            this.middleName = middleName;
+            return  this;
+        }
+        public Builder setLastName(String lastName){
+            this.lastName = lastName;
+            return  this;
+        }
+        public Builder copy(Name name){
+            this.firstName = name.firstName;
+            this.middleName = name.middleName;
+            this.lastName = name.lastName;
+            return this;
+        }
+        public Name build(){
+            return  new Name(this);
+
+        }
+    }
 }
