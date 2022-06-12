abe5e37c96c9433fd34aac07442d081fd37ff983
Chad Siegelaar <218340982@mycput.ac.za>
2022-06-12 22:04:16 +0200

    Employee Factory

---
+++ src/test/java/za/ac/cput/school_management/factory/EmployeeFactoryTest.java
@@ -0,0 +1,28 @@
+/*
+ *Author:Chad Siegelaar 218340982
+ *Employee Factory Test
+ *11 June 2022
+ */
+
+package za.ac.cput.school_management.factory;
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.school_management.domain.Employee;
+import za.ac.cput.school_management.domain.Name;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EmployeeFactoryTest {
+    @Test
+    public void buildWithSuccess() {
+        Employee employee = EmployeeFactory.build("011", "employee@inet.co.za", new Name);
+        System.out.println(employee);
+        assertNotNull(employee);
+    }
+    @Test
+    public void  buildWithError() {
+        Exception exception = assertThrows(IllegalArgumentException.class,() ->
+                EmployeeFactory.build(null, "Daniel Smith", new Name));
+        assertTrue(exception.getMessage().contains("Staff Id is needed"));
+    }
+}
