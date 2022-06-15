8946f063045cdebae6d49b921336d66cf125bf55
Bratigo16 <219053715@mycput.ac.za>
2022-06-15 19:06:13 +0200

    committing question 1 - 3

---
+++ src/test/java/za/ac/cput/school_management/factory/StudentFactoryTest.java
@@ -0,0 +1,42 @@
+package za.ac.cput.school_management.factory;
+/*
+ *  Author: Brady Sheldon 219053715
+ *  Student
+ *  June Assignment
+ *  Date: 09 June 2022
+ * */
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.school_management.domain.Student;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentFactoryTest {
+
+    @Test
+    void buildWithError(){
+        Exception exception = assertThrows(IllegalAccessException.class,() ->
+                StudentFactory.build("219067988",
+                        "219067988@mycput.ac.za",
+                        "Frank",
+                        "Overrated",
+                        "Lampard"));
+        System.out.println(exception.getMessage());
+        assertTrue(exception.getMessage().contains("StudentID"));
+    }
+
+    @Test
+    void buildWithSuccess(){
+        Student student = StudentFactory
+                .build("219067988",
+                        "219067988@mycput.ac.za",
+                        "Thierry",
+                        "Goat",
+                        "Henry");
+        System.out.println(student);
+        assertAll(
+                () -> assertNotNull(student)
+        );
+    }
+}
