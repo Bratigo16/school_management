63723ff41c034426fc82ae6c00fa0a611bc99571
Wilbur Smith <220003033@mycput.ac.za>
2022-06-17 22:19:18 +0200

    Added Country Service tests

--- src/test/java/za/ac/cput/school_management/service/CountryServiceTest.java
+++ src/test/java/za/ac/cput/school_management/service/CountryServiceTest.java
@@ -1,24 +1,58 @@
+/*  CountryServiceTest.java
+    Class to test the Country Service
+    Author: Wilbur Deano Smith (220003033)
+    Date: 17 June 2022
+*/
 package za.ac.cput.school_management.service;
 
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import za.ac.cput.school_management.domain.Country;
+import za.ac.cput.school_management.domain.StudentAddress;
+import za.ac.cput.school_management.factory.CountryFactory;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CountryServiceTest {
 
+    private final Country country = CountryFactory
+            .build("test-id",
+                    "test-name");
+
+    @Autowired
+    private CountryService service;
+
     @Test
-    void save() {
+    void save()
+    {
+        Country saved = this.service.save(this.country);
+        assertAll(
+                () -> assertNotNull(saved),
+                () -> assertEquals(this.country, saved)
+        );
     }
 
     @Test
     void read() {
-    }
-
-    @Test
-    void reading() {
+        Optional<Country> read = this.service.read(this.country.getId());
+        assertAll(
+                () -> assertTrue(read.isPresent()),
+                () -> assertSame(this.country, read.get())
+        );
     }
 
     @Test
     void delete() {
+        this.service.delete(this.country);
+    }
+
+    @Test
+    void findAll() {
+        List<Country> countryList = this.service.findAll();
+        assertSame(0, countryList);
     }
 }
