ceca5ca9cf31720e4cb2466f0f486de0cbf48f2b
Bratigo16 <219053715@mycput.ac.za>
2022-06-17 20:22:53 +0200

    committing question 1 - 4

--- src/main/java/za/ac/cput/school_management/repository/IStudentRepository.java
+++ src/main/java/za/ac/cput/school_management/repository/IStudentRepository.java
@@ -6,11 +6,12 @@ package za.ac.cput.school_management.repository;
  *  Date: 09 June 2022
  * */
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import za.ac.cput.school_management.domain.Student;
 
 import java.util.List;
 
-public interface IStudentRepository extends IRepository<Student, String> {
+public interface IStudentRepository extends JpaRepository<Student, String> {
 
 
     List<Student> findAll();
