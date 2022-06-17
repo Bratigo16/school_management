cb303e8c9b116ad5404121f55446f10a88cf3c53
Bratigo16 <219053715@mycput.ac.za>
2022-06-17 20:33:16 +0200

    committing question 1 - 4

---
+++ src/main/java/za/ac/cput/school_management/service/IStudentService.java
@@ -0,0 +1,22 @@
+package za.ac.cput.school_management.service;
+
+/*
+ *  Author: Brady Sheldon 219053715
+ *  Student
+ *  June Assignment
+ *
+ * */
+
+import za.ac.cput.school_management.domain.Student;
+
+
+import java.util.List;
+
+
+public interface IStudentService extends IService<Student, String> {
+
+    public List<Student> findAll();
+
+
+
+}
